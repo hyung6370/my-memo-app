dd471cf963ea535e1f8383ec62b2b5407eafb31e
emptyhead <korea6370@yu.ac.kr>
2023-07-18 23:50:53 +0900

    Initial Commit

---
+++ MyToDoApp/Models/ToDoData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoData+CoreDataClass.swift
+//  MyToDoApp
+//
+//  Created by KIM Hyung Jun on 2023/07/18.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoData)
+public class ToDoData: NSManagedObject {
+
+}
