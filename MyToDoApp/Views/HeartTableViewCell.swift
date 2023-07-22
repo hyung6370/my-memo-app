7ff8a1ce8b3ab5b9c8a5cf4b0274a1fa898fbecd
emptyhead <korea6370@yu.ac.kr>
2023-07-22 17:07:51 +0900

    💜 [Add] : 하트 리스트 구현 중!

---
+++ MyToDoApp/Views/HeartTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HeartTableViewCell.swift
+//  MyToDoApp
+//
+//  Created by KIM Hyung Jun on 2023/07/22.
+//
+
+import UIKit
+
+class HeartTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
